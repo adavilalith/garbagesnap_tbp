f8fa23715d4092c2868b8d662a4aae65ba7f77f4
Akshith1213 <126947319+Akshith1213@users.noreply.github.com>
2024-02-10 06:36:07 +0000

    adding links to github, documentation and presentation in the fooeter links section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,9 +20,9 @@ export default function App() {
         <Col className='text-white ms-5'>
           <Row className='mb-4'><p className='h4'>Links</p></Row>
           <Row className='mt-2 '>
-            <a href='' className='my-2' style={{textDecoration:'none',color:'white'}}>GitHub</a>
-            <a href='' className='my-2' style={{textDecoration:'none',color:'white'}}>Documentation</a>
-            <a href='' className='my-2' style={{textDecoration:'none',color:'white'}}>Presentation</a>
+            <a href='https://github.com/adavilalith/garbagesnap_tbp' target='_balnk' className='my-2' style={{textDecoration:'none',color:'white'}}>GitHub</a>
+            <a href='https://docs.google.com/file/d/1HOsPy9zp4uzv8GsxdvbjhuUeHcds2GoO/edit?usp=docslist_api&filetype=msword' target='_balnk' className='my-2' style={{textDecoration:'none',color:'white'}}>Documentation</a>
+            <a href='https://docs.google.com/file/d/1DRETiOq0_eC8D3ZsWIvpo8wuFQbU61do/edit?usp=docslist_api&filetype=mspresentation' target='_balnk' className='my-2' style={{textDecoration:'none',color:'white'}}>Presentation</a>
           </Row>
         </Col>
         <Col className='text-white'>
